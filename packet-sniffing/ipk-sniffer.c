98902eb2c94f760cfe5c2e73855cc5222f6c41d5
sabgul <sabina.gulcikova@gmail.com>
2021-04-24 13:38:51 +0200

    listing interfaces

--- packet-sniffing/ipk-sniffer.c
+++ packet-sniffing/ipk-sniffer.c
@@ -2,13 +2,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <pcap.h>
 
 #define SUCCESS 0
 #define ARG_ERROR 11
+#define FINDALLDEVS_ERR 2
 #define INTERNAL_ERR 99
 
 // program by sa mal dat kedykolvek ukoncit pomocou ctrl+c
 // TODO mozno povolit kombinovanie filtrov, to dorobim ak ostane cas, inak dovolujem len jeden
+int listInterfaces() {
+    char errorBuffer[PCAP_ERRBUF_SIZE];
+    pcap_if_t *interfaceList;
+    pcap_if_t *iElement;
+    
+    int findInterfacesCheck = pcap_findalldevs(&interfaceList, errorBuffer);
+    if (findInterfacesCheck != 0) {
+        fprintf(stderr, "error: findalldevs failed\n");
+        return FINDALLDEVS_ERR;
+    } 
+
+    if (interfaceList == NULL) {
+        fprintf(stdout, "There are no active interfaces.\n");
+        return SUCCESS;
+    }
+
+    iElement = interfaceList;
+    while(iElement != NULL) {
+        fprintf(stdout, "%s\n", iElement->name);
+        iElement = iElement->next;
+    }
+
+    return SUCCESS;
+}
+
+
 int main(int argc, char **argv) {
     /* Flags specify whether given argument was entered / specified, or not */
     bool interfaceSet = false; /* defines, whether -i option was used */
@@ -19,7 +47,7 @@ int main(int argc, char **argv) {
     bool arpSpec = false; /* lists only arp frames */
     bool icmpSpec = false; /* lists only ICMPv4 and ICMPv6 packets */
     bool numSpec = false; 
-    int numOfPackets = -1;
+    int numOfPackets = 1;
     int port = -1;
     char *interface = NULL;
 
@@ -38,7 +66,7 @@ int main(int argc, char **argv) {
                     interface = malloc(strlen(argv[1]) + 1);
 
                     if(interface == NULL) {
-                        fprintf(stderr, "error: allocation failed.");
+                        fprintf(stderr, "error: allocation failed.\n");
                         return INTERNAL_ERR;
                     }
 
@@ -56,6 +84,12 @@ int main(int argc, char **argv) {
                 fprintf(stderr, "error: -p option was used but not specified\n"); return ARG_ERROR; 
             }
             port = atoi(argv[i]);
+
+            if (port < 0 || port > 65635) {
+                fprintf(stderr, "error: invalid port\n");
+                return ARG_ERROR;
+            }
+
         } else if (strcmp(argv[i], "-n") == 0) {
             numSpec = true;
             i++;
@@ -98,5 +132,22 @@ int main(int argc, char **argv) {
     }
     /*-------------------------------------------------------------------------------------*/
     
+    // get available devices
+    if (interfaceSet == false || interfaceSpec == false) {
+        return listInterfaces();
+    } else {
+        // tuto budeme snifovac
+    }
+    // TODO skontrolovat ci je zadany interface v liste vstekych rozhrani
+    // set up sniffing 
+
+
+    /*--------------------------------------CLEANUP-----------------------------------------*/
+
+    if (interfaceSpec) {
+        free(interface);
+    }
+
     return SUCCESS;
 }
+    /*-------------------------------------END OF CODE--------------------------------------*/
