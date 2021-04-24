770cf48c13d30deed3d96af281141147daa98645
sabgul <sabina.gulcikova@gmail.com>
2021-04-24 10:47:35 +0200

    polish processing of arguments

--- packet-sniffing/ipk-sniffer.c
+++ packet-sniffing/ipk-sniffer.c
@@ -5,43 +5,15 @@
 
 #define SUCCESS 0
 #define ARG_ERROR 11
+#define INTERNAL_ERR 99
 
-int processArguments(int argc, char **argv, bool *iSpec, char **interface, bool *numSpec, int *numOfPackets, bool *portSpec, int *port) {
-    // *numOfPackets = 6;
-    for(int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--interface") == 0) {
-            // prekopirujem hodnotu z i+1 do interface, i inkrementujem, skontrolujem ci nie je vacsie ako argc
-            i++;
-            if(i >= argc) { return ARG_ERROR; }
-            // strcpy 
-        } else if (strcmp(argv[i], "-p") == 0) {
-            i++;
-            if(i >= argc) { return ARG_ERROR; }
-            *port = atoi(argv[i]);
-        } else if (strcmp(argv[i], "-n") == 0) {
-            i++;
-            if(i >= argc) { return ARG_ERROR; }
-            *numOfPackets = atoi(argv[i]);
-        } else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--tcp") == 0 ||
-                   strcmp(argv[i], "-u") == 0 || strcmp(argv[i], "--udp") == 0 ||
-                   strcmp(argv[i], "--arp") == 0 ||
-                   strcmp(argv[i], "--icmp") == 0) {
-                   continue;
-        } else {
-            return ARG_ERROR;
-        }
-    }
-
-    // skontrolovat ci mam len povolene argumenty, skontrolovat ci mam spravne cislo portu
-
-    return SUCCESS;
-}
-
+// program by sa mal dat kedykolvek ukoncit pomocou ctrl+c
 
 int main(int argc, char **argv) {
     /* Flags specify whether given argument was entered / specified, or not */
-    bool interfaceSpec = false;
-    bool portSpec = false;
+    bool interfaceSet = false; /* defines, whether -i option was used */
+    bool interfaceSpec = false; /* defines, whether interface was specified */
+    bool portSpec = false; /* defines, whether -p option was used an parameter specified */
     bool tcpSpec = false;
     bool udpSpec = false;
     bool arpSpec = false;
@@ -51,29 +23,49 @@ int main(int argc, char **argv) {
     int port = -1;
     char *interface = NULL;
 
-    /* Processing arguments which specify the desired behaviour of the program */
-    // if(processArguments(argc, argv, &interfaceSpec, &interface, &numSpec, &numOfPackets, &portSpec, &port) == ARG_ERROR) {
-    //     fprintf(stderr, "error: invalid arguments were entered\n");
-    //     return ARG_ERROR;
-    // }
-
+    /*------------------------------ PROCESSING OF ARGUMENTS ------------------------------*/
+    /* Checking if only allowed arguments and their combinations were entered, respective flags are set */
     for(int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--interface") == 0) {
-            interfaceSpec = true; 
+            interfaceSet = true; 
             i++;
-            if(i >= argc) { fprintf(stderr, "error: invalid arguments were entered\n"); return ARG_ERROR; }
-            // TODO strcpy 
-            strcpy(interface, argv[i]);
+            if(i < argc) {
+                if (!(strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "-n") == 0 ||
+                    strcmp(argv[i], "--tcp") == 0 || strcmp(argv[i], "-t") == 0 ||
+                    strcmp(argv[i], "--udp") == 0 || strcmp(argv[i], "-u") == 0 ||
+                    strcmp(argv[i], "--arp") == 0 || strcmp(argv[i], "--icmp") == 0)) {
+                    interfaceSpec = true; 
+                    interface = malloc(strlen(argv[1]) + 1);
+
+                    if(interface == NULL) {
+                        fprintf(stderr, "error: allocation failed.");
+                        return INTERNAL_ERR;
+                    }
+
+                    strcpy(interface, argv[i]);
+                } 
+            }
         } else if (strcmp(argv[i], "-p") == 0) {
             portSpec = true;
             i++;
-            if(i >= argc) { fprintf(stderr, "error: invalid arguments were entered\n"); return ARG_ERROR; }
+            if((i >= argc) || strcmp(argv[i], "-n") == 0 || 
+                    strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--interface") == 0 ||
+                    strcmp(argv[i], "--tcp") == 0 || strcmp(argv[i], "-t") == 0 ||
+                    strcmp(argv[i], "--udp") == 0 || strcmp(argv[i], "-u") == 0 ||
+                    strcmp(argv[i], "--arp") == 0 || strcmp(argv[i], "--icmp") == 0) { 
+                fprintf(stderr, "error: -p option was used but not specified\n"); return ARG_ERROR; 
+            }
             port = atoi(argv[i]);
-            // TODO check ci ide o valid port
         } else if (strcmp(argv[i], "-n") == 0) {
             numSpec = true;
             i++;
-            if(i >= argc) { fprintf(stderr, "error: invalid arguments were entered\n"); return ARG_ERROR; }
+            if(i >= argc || strcmp(argv[i], "-p") == 0 || 
+                    strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--interface") == 0 ||
+                    strcmp(argv[i], "--tcp") == 0 || strcmp(argv[i], "-t") == 0 ||
+                    strcmp(argv[i], "--udp") == 0 || strcmp(argv[i], "-u") == 0 ||
+                    strcmp(argv[i], "--arp") == 0 || strcmp(argv[i], "--icmp") == 0) { 
+                fprintf(stderr, "error: -n option was used but not specified\n"); return ARG_ERROR; 
+            }
             numOfPackets = atoi(argv[i]);
         } else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--tcp") == 0) {
             tcpSpec = true;
@@ -88,7 +80,7 @@ int main(int argc, char **argv) {
             return ARG_ERROR;
         }
     }
-
-    /* sets flags to true if user specified so */
+    /*-------------------------------------------------------------------------------------*/
+    
     return SUCCESS;
 }
